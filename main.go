2db60bcd48c27845b2fdb742918025e94a9c4d77
Raif Atef <beliskner.github.3psil0n@neverbox.com>
2024-09-26 21:48:16 +0300

    Fix output truncation

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func execCommandAndGetResponse(command string, args ...string) string {
 		return "<pre>" + errorHtml + "</pre>"
 	} else {
 		output := string(out.Bytes())
-		output = strings.Replace(output, "\nOK\n", "", 1)
+		output = strings.Replace(output, "\r\nOK\r\n", "", 1)
 		output = strings.Trim(output, "\r\n \t")
 		return "<pre>" + output + "</pre>"
 	}
